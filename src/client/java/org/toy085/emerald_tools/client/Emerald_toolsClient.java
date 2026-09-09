0e64faabc989d22607b33341d6e831705c1f21d8
Toy085 <brightabaka1@gmail.com>
2026-09-09 14:08:45 +0200

    Add template for minecraft mod

---
+++ src/client/java/org/toy085/emerald_tools/client/Emerald_toolsClient.java
@@ -0,0 +1,10 @@
+package org.toy085.emerald_tools.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Emerald_toolsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/toy085/emerald_tools/Emerald_tools.java
@@ -0,0 +1,10 @@
+package org.toy085.emerald_tools;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Emerald_tools implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
