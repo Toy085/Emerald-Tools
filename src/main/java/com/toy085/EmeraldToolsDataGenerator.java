b2449e7cc7dd5e8adfd75bdf5761595e90609e6c
Toy085 <brightabaka1@gmail.com>
2026-09-09 15:08:00 +0200

    Update mod template

---
+++ src/main/java/com/toy085/EmeraldToolsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.toy085;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EmeraldToolsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
